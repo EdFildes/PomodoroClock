cc256f3231c419b3837f8bfbd6a3777f9f273f88
Ed <eddfildes@hotmail.com>
2019-09-11 17:53:24 +0100

    basic layout and styling done with html and bulma.css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import './css/bulma.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container" class="container">
+
+      <div id="secondaryContainerA" class="secondaryContainerA">
+
+        <div id="studyTimerContainer" class="timerContainer">
+          <h1 class="timerTitle content is-large">Study</h1>
+          <input id="studyTimer" type="time" class="input is-primary is-large" value="10:00"></input>
+          <div id="buttonContainer" class="buttonContainer">
+            <button id="incStudyTimer" class="button is-primary">+5 mins</button>
+            <button id="decStudyTimer" class="button is-primary">-5 mins</button>
+          </div>
+        </div>
+      
+
+        <div id="studyTimerContainer" class="timerContainer">
+          <h1 class="timerTitle content is-large">Break</h1>
+          <input id="breakTimer" type="time" class="input is-primary is-large" value="05:00"></input>
+          <div id="buttonContainer" class="buttonContainer">
+            <button id="incBreakTimer" class="button is-primary">+5 mins</button>
+            <button id="decBreakTimer" class="button is-primary">-5 mins</button>
+          </div>
+        </div>
+      </div>
+
+      <div id="secondaryContainerB" class="secondaryContainerB">
+        <time id="timer" datetime="15:00" class="output content is-large">15:00</time>
+
+        <div class="radioContainer">
+          <input id="isStudyTimer" type="radio" checked="true" name="timerType"></input>
+          <input id="isBreakTimer" type="radio" name="timerType"></input>
+        </div>
+      </div>
+
     </div>
   );
 }
